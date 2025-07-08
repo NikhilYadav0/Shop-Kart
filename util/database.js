db2c86ddcaac13483658837f1c33f286d6be4f21
Nikhil Yadav <nikhily.725@gmail.com>
2025-07-08 17:05:53 +0530

    8/7/2025

--- util/database.js
+++ util/database.js
@@ -7,17 +7,8 @@ var sequelize = new Sequelize("shop-kart", "star", "userpass", {
     max: 5,
     min: 0,
     idle: 10000,
-  },
-});
+  },  
+}); 
 
 module.exports = sequelize;
-// const mysql = require("mysql2");
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "shop-kart",
-//   password: "star.2706"
-// });
-
-// module.exports = pool.promise();
